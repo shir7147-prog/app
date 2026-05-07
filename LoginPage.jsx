5549d0d8fc4c19cdaac98bd2123cee3d014f7afa
shir7147-prog <shir7147@gmail.com>
2026-05-07 12:35:55 +0300

    Add LoginPage component with login form

---
+++ LoginPage.jsx
@@ -0,0 +1,17 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <div className="card">
+        <input placeholder="Email" />
+        <br />
+        <br />
+        <input placeholder="Password" type="password" />
+        <br />
+        <br />
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
