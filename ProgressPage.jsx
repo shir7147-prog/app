cdc2651860d21aece1f423d3655b0200b075cbba
shir7147-prog <shir7147@gmail.com>
2026-05-07 12:37:11 +0300

    Create ProgressPage component with initial layout

---
+++ ProgressPage.jsx
@@ -0,0 +1,16 @@
+export default function ProgressPage() {
+  return (
+    <div>
+      <h1>Progress</h1>
+
+      <div className="card">
+        <p>Weight: 70kg</p>
+        <p>BMI: 22</p>
+      </div>
+
+      <div className="card">
+        <p>Progress Chart (Placeholder)</p>
+      </div>
+    </div>
+  );
+}
