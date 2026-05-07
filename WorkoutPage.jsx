0d3d65011c2883fa529ea583ce194d48d25d0383
shir7147-prog <shir7147@gmail.com>
2026-05-07 12:36:23 +0300

    Create WorkoutPage component with workout options

---
+++ WorkoutPage.jsx
@@ -0,0 +1,13 @@
+export default function WorkoutPage() {
+  return (
+    <div>
+      <h1>Workouts</h1>
+
+      <div className="card">Cardio</div>
+      <div className="card">Strength</div>
+      <div className="card">Yoga</div>
+
+      <button>Start Workout</button>
+    </div>
+  );
+}
